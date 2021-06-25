50ce2f0b55f7f9d613cf6f5de0e5c13cccdcbcec
Antonioflo13 <floreantonio@hotmail.it>
2021-06-25 16:56:13 +0200

    strlower for all key

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ $censuredWord = "";
 $string = "Tenente Aldo Raine: Se hai sentito parlare di noi sai che il nostro lavoro non è fare prigionieri, ma uccidere i nazisti. E gli affari vanno a meraviglia. 
 //Brad Pitt, Bastardi senza gloria.//";
 echo $string . " "  . "<br>" . "//Lughezza caratteri citazione" . " "  . strlen($string) . "<br>";
-echo "//Parola censurata:" . " " . "<strong>" . $censuredWord . "</strong>". "<br>";
-echo $newString = str_replace(ucfirst($_GET["censuredWord"]), "***", $string) . "<br>";
+echo "//Parola censurata:" . " " . "<strong>" . $_GET["censuredWord"] . "</strong>". "<br>";
+$newString = str_replace($_GET["censuredWord"], "***", strtolower($string)) . "<br>";
+echo $newString;
 echo strlen($newString) . "<br>";
 ?>
